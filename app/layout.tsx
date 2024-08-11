9f2445e05fd3e0e8fc8c598c807004422c916fea
Alastair Deng <adeng27@stanford.edu>
2024-08-10 21:54:25 -0700

    ui for register

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import ClientOnly from "./components/ClientOnly";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -18,8 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={nunito.className}>
-        <Navbar />
-        {children}
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+          {children}
+        </ClientOnly>
       </body>
     </html>
   );
