98ab7e9f88126ba3dfaf9f6e9b787d67fc8fe306
Alastair Deng <adeng27@stanford.edu>
2024-08-27 22:15:37 -0700

    image upload!

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
             },
             {
                 hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                hostname: 'res.cloudinary.com',
             }
         ]
     }
