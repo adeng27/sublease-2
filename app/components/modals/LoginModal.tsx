4202b46f3f4a2887aa5ef91a9bd2710a2afe9ce2
Alastair Deng <adeng27@stanford.edu>
2024-08-27 21:31:44 -0700

    fix for deployment

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -104,7 +104,7 @@ const LoginModal = () => {
             <div className="text-neutral-500 text-center mt-4 font-light">
                 <div className="justify-center flex flex-row items-center gap-2">
                     <div>
-                        Don't have an account? 
+                        Don&apos;t have an account? 
                     </div>
                     <div 
                         onClick={toggle}
